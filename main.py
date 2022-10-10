6dfd6dd6373f643e772bfb09fd8f227c710b5bce
Carrie Wang <carrie07@hku.hk>
2022-10-10 17:36:24 +0800

    Update Output of Ravdess AO

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from utils.opts import parse_opt
-from utils.util import normalize_features,loss_plot,clean,setup_seed,saveModel,loadModel,confusion_matrix_plot
+from utils.util import normalize_features,loss_plot,clean,setup_seed,saveModel,loadModel,confusion_matrix_plot, logger,acc_plot
 from feature_extraction import get_features,load_features
-from model import ParallelModel,make_train_step,make_validate_fnc,loss_fnc
+from model import AV_transformer,AO_transformer, VO_transformer, make_train_step,make_validate_fnc,loss_fnc
 import numpy as np
 import torch
 import time
@@ -10,6 +10,7 @@ def main():
     clean()
     config = parse_opt()
     setup_seed(config)
+    logger(config)
 
     if config.dataset=="Crema":
         config.Emotions_map= {'NEU':'neutral',
@@ -42,35 +43,48 @@ def main():
 
     t0=time.time()
     if config.feature_exist == True:
-        X1_train,X2_train,Y_train = load_features(config,"train.p")
-        # X1_val,X2_val,Y_val = load_features(config,"val.p")
-        X1_test,X2_test,Y_test = load_features(config,"test.p")
+        X1_train,X2_train,Z_train, Y_train, G_train = load_features(config,"train.p")
+        X1_test,X2_test,Z_test, Y_test, G_test = load_features(config,"test.p")
     else:
-        X1_train,X2_train,Y_train,X1_val,X2_val,Y_val,X1_test,X2_test,Y_test = get_features(config, train=True)
+        get_features(config, train=True)
+        X1_train,X2_train,Z_train, Y_train, G_train = load_features(config,"train.p")
+        X1_test,X2_test,Z_test, Y_test, G_test = load_features(config,"test.p")
 
-    X1_train,X1_test = normalize_features(X1_train,X1_test,spec=False)
+    X1_train,X1_test = normalize_features(X1_train,X1_test,audio_features_flag=True)
     X2_train,X2_test = normalize_features(X2_train,X2_test)
+    Z_train,Z_test = normalize_features(Z_train,Z_test)
 
     DATASET_SIZE = X1_train.shape[0]
-    config.device = 'cpu'#'cuda' if torch.cuda.is_available() else 'cpu'
-    print('Selected device is {}'.format(config.device))
-    model = ParallelModel(num_emotions=len(config.Emotions_map),dropout=config.dropout,rnn_size=config.rnn_size).to(config.device)
+    config.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    config.logger.info('Selected device is {}'.format(config.device))
+
+    if config.mode =='AV':
+        model = AV_transformer(num_emotions=len(config.Emotions_map),dropout=config.dropout,dim_feedforward=config.dim_feedforward).to(config.device)
+    elif config.mode == 'AO':
+        model = AO_transformer(num_emotions=len(config.Emotions_map),dropout=config.dropout,dim_feedforward=config.dim_feedforward).to(config.device)
+    elif config.mode =='VO':
+        model = VO_transformer(num_emotions=len(config.Emotions_map),dropout=config.dropout,dim_feedforward=config.dim_feedforward).to(config.device)
     OPTIMIZER = torch.optim.AdamW(model.parameters(),lr=config.lr, weight_decay=config.weight_decay)
 
     if config.pretrained == True:
         model,OPTIMIZER = loadModel(config,model,OPTIMIZER)
-        print('Finished preloading!')
+        config.logger.info('Finished preloading! Pretrained model is used.')
+        print('Finished preloading! Pretrained model is used.')
 
     train_step = make_train_step(model, loss_fnc, optimizer=OPTIMIZER)
     validate = make_validate_fnc(model,loss_fnc)
-    losses=[]
-    val_losses = []
+
+    # start training
+    best_accuracy = 0.0
+    losses,accuracies=[],[]
+    test_losses,test_accuracies = [],[]
     for epoch in range(config.epochs):
         t_epoch=time.time()
         # schuffle data
         ind = np.random.permutation(DATASET_SIZE)
         X1_train = X1_train[ind,:,:]
         X2_train = X2_train[ind,:,:,:]
+        Z_train = Z_train[ind,:,:,:]
         Y_train = Y_train[ind]
         epoch_acc = 0.0
         epoch_loss = 0.0
@@ -81,36 +95,47 @@ def main():
             actual_batch_size = batch_end-batch_start
             X1 = X1_train[batch_start:batch_end,:,:]
             X2 = X2_train[batch_start:batch_end,:,:,:]
+            Z = Z_train[batch_start:batch_end,:,:,:]
             Y = Y_train[batch_start:batch_end]
             X1_tensor = torch.tensor(X1,device=config.device).float()
             X2_tensor = torch.tensor(X2,device=config.device).float()
+            Z_tensor = torch.tensor(Z,device=config.device).float()
             Y_tensor = torch.tensor(Y, dtype=torch.long,device=config.device)
-            loss, acc = train_step(X1_tensor,X2_tensor,Y_tensor)
+            loss, acc = train_step(X1_tensor,X2_tensor,Z_tensor, Y_tensor)
             epoch_acc += acc*actual_batch_size/DATASET_SIZE
             epoch_loss += loss*actual_batch_size/DATASET_SIZE
-            print(f"\r Epoch {epoch}: iteration {i}/{iters}",end='')
+            config.logger.info(f"\r Epoch {epoch+1}: iteration {i+1}/{iters}")
+            print(f"\r Epoch {epoch+1}: iteration {i+1}/{iters}",end='')
 
-        # X1_val_tensor = torch.tensor(X1_val,device=config.device).float()
-        # X2_val_tensor = torch.tensor(X2_val,device=config.device).float()
-        # Y_val_tensor = torch.tensor(Y_val,dtype=torch.long,device=config.device)
         X1_test_tensor = torch.tensor(X1_test,device=config.device).float()
         X2_test_tensor = torch.tensor(X2_test,device=config.device).float()
+        Z_test_tensor = torch.tensor(Z_test,device=config.device).float()
         Y_test_tensor = torch.tensor(Y_test,dtype=torch.long,device=config.device)
-        val_loss, val_acc, _ = 0,0,0 #validate(X1_val_tensor,X2_val_tensor,Y_val_tensor)
-        test_loss, test_acc, test_predictions = validate(X1_test_tensor,X2_test_tensor,Y_test_tensor)
+        test_loss, test_acc, test_predictions = validate(X1_test_tensor,X2_test_tensor, Z_test_tensor,Y_test_tensor)
 
         losses.append(epoch_loss)
-        val_losses.append(test_loss)
+        test_losses.append(test_loss)
+        accuracies.append(acc)
+        test_accuracies.append(test_acc)
+        config.logger.info('')
+        config.logger.info(f"Epoch {epoch+1} --> loss:{epoch_loss:.4f}, acc:{epoch_acc:.2f}%, test_loss:{test_loss:.4f}, test_acc:{test_acc:.2f}%, Time per epoch: {(time.time()-t_epoch):.4f}")
         print('')
-        print(f"Epoch {epoch} --> loss:{epoch_loss:.4f}, acc:{epoch_acc:.2f}%, val_loss:{val_loss:.4f}, val_acc:{val_acc:.2f}%, Time per epoch: {(time.time()-t_epoch):.4f}")
-        print(f"--- --- --> test_loss:{test_loss:.4f}, test_acc:{test_acc:.2f}%")
-        if epoch == config.epochs - 1:
-            print(len(Y_test))
-            print(len(test_predictions))
-            confusion_matrix_plot(config,Y_test,test_predictions)
-            loss_plot(config, epoch+1,losses, val_losses)
+        print(f"Epoch {epoch+1} --> loss:{epoch_loss:.4f}, acc:{epoch_acc:.2f}%, test_loss:{test_loss:.4f}, test_acc:{test_acc:.2f}%, Time per epoch: {(time.time()-t_epoch):.4f}")
+
+        # save the model with the highest test accuracy
+        if test_acc>best_accuracy:
+            best_accuracy = test_acc
             saveModel(config,model,OPTIMIZER)
-            print(f'Model saved! Total Time: {(time.time()-t0):.4f}')
+            config.logger.info(f"The best model up to now is saved with test accuracy:{test_acc:.2f}% at epoch: {epoch+1}/{config.epochs}")
+            print(f"The best model up to now is saved with test accuracy:{test_acc:.2f}% at epoch: {epoch+1}/{config.epochs}")
+
+        # save all plots, confusion matrix and overall time spend in the end
+        if epoch == config.epochs - 1:
+            confusion_matrix_plot(config,Y_test,test_predictions)
+            loss_plot(config, epoch+1,losses, test_losses)
+            acc_plot(config, epoch+1,accuracies, test_accuracies)
+            config.logger.info(f'Finished training! Total Time: {(time.time()-t0):.4f}')
+            print(f'Finished training! Total Time: {(time.time()-t0):.4f}')
 
 
 
--- model.py
+++ model.py
@@ -2,8 +2,96 @@
 import torch
 import torch.nn as nn
 
-class ParallelModel(nn.Module):
-    def __init__(self,num_emotions,dropout,rnn_size):
+class AV_transformer(nn.Module):
+    def __init__(self,num_emotions,dropout,dim_feedforward):
+        super().__init__()
+        # Audio Transformer block
+        self.transf_maxpool = nn.MaxPool2d(kernel_size=[2,4], stride=[2,4])
+        transf_layer = nn.TransformerEncoderLayer(d_model=64, nhead=4, dim_feedforward=dim_feedforward, dropout=dropout, activation='relu')
+        self.transf_encoder = nn.TransformerEncoder(transf_layer, num_layers=2)
+
+        # Facial Transformer block
+        self.transf_maxpool_facial = nn.MaxPool2d(kernel_size=[1,4], stride=[1,2])
+        transf_layer_facial = nn.TransformerEncoderLayer(d_model=35, nhead=5, dim_feedforward=dim_feedforward, dropout=dropout, activation='relu')
+        self.transf_encoder_facial = nn.TransformerEncoder(transf_layer_facial, num_layers=2)
+
+        # Linear softmax layer
+        # self.linear_in = hidden_size
+        self.out_linear = nn.LazyLinear(num_emotions) #2*hidden_size+192,
+        self.dropout_linear = nn.Dropout(p=dropout)
+        self.out_softmax = nn.Softmax(dim=1)
+    def forward(self,x1,x2,z):
+
+        # transformer embedding for audios
+        x_reduced = self.transf_maxpool(x2)
+        x_reduced = torch.squeeze(x_reduced,1)
+        x_reduced = x_reduced.permute(2,0,1) # requires shape = (time,batch,embedding)
+        transf_out = self.transf_encoder(x_reduced)
+        transf_embedding = torch.mean(transf_out, dim=0)
+
+        # transformer embedding for videos
+        z_reduced = self.transf_maxpool_facial(z)
+        z_reduced = torch.squeeze(z_reduced,1)
+        z_reduced = z_reduced.permute(2,0,1) # requires shape = (time,batch,embedding)
+        z_transf_out = self.transf_encoder_facial(z_reduced)
+        z_transf_embedding = torch.mean(z_transf_out, dim=0)
+
+
+        # concatenate with extracted features
+        complete_embedding = torch.cat([transf_embedding,z_transf_embedding,x1.squeeze()],dim=1)
+        
+        # self.linear_in = complete_embedding.shape[1]
+        output_logits = self.out_linear(complete_embedding)
+        output_logits = self.dropout_linear(output_logits)
+        output_softmax = self.out_softmax(output_logits)
+        return output_logits, output_softmax#, attention_weights_norm
+
+class VO_transformer(nn.Module):
+    def __init__(self,num_emotions,dropout,dim_feedforward):
+        super().__init__()
+        # Audio Transformer block
+        self.transf_maxpool = nn.MaxPool2d(kernel_size=[2,4], stride=[2,4])
+        transf_layer = nn.TransformerEncoderLayer(d_model=64, nhead=4, dim_feedforward=dim_feedforward, dropout=dropout, activation='relu')
+        self.transf_encoder = nn.TransformerEncoder(transf_layer, num_layers=4)
+
+        # Facial Transformer block
+        self.transf_maxpool_facial = nn.MaxPool2d(kernel_size=[1,4], stride=[1,2])
+        transf_layer_facial = nn.TransformerEncoderLayer(d_model=35, nhead=5, dim_feedforward=dim_feedforward, dropout=dropout, activation='relu')
+        self.transf_encoder_facial = nn.TransformerEncoder(transf_layer_facial, num_layers=4)
+
+        # Linear softmax layer
+        # self.linear_in = hidden_size
+        self.out_linear = nn.LazyLinear(num_emotions) #2*hidden_size+192,
+        self.dropout_linear = nn.Dropout(p=dropout)
+        self.out_softmax = nn.Softmax(dim=1)
+    def forward(self,x1,x2,z):
+
+        # # transformer embedding for audios
+        # x_reduced = self.transf_maxpool(x2)
+        # x_reduced = torch.squeeze(x_reduced,1)
+        # x_reduced = x_reduced.permute(2,0,1) # requires shape = (time,batch,embedding)
+        # transf_out = self.transf_encoder(x_reduced)
+        # transf_embedding = torch.mean(transf_out, dim=0)
+
+        # transformer embedding for videos
+        z_reduced = self.transf_maxpool_facial(z)
+        z_reduced = torch.squeeze(z_reduced,1)
+        z_reduced = z_reduced.permute(2,0,1) # requires shape = (time,batch,embedding)
+        z_transf_out = self.transf_encoder_facial(z_reduced)
+        z_transf_embedding = torch.mean(z_transf_out, dim=0)
+
+
+        # concatenate with extracted features
+        complete_embedding = z_transf_embedding
+        
+        # self.linear_in = complete_embedding.shape[1]
+        output_logits = self.out_linear(complete_embedding)
+        output_logits = self.dropout_linear(output_logits)
+        output_softmax = self.out_softmax(output_logits)
+        return output_logits, output_softmax#, attention_weights_norm
+
+class AO_transformer(nn.Module):
+    def __init__(self,num_emotions,dropout, dim_feedforward):
         super().__init__()
         # conv block
         self.conv2Dblock = nn.Sequential(
@@ -52,54 +140,78 @@ class ParallelModel(nn.Module):
             nn.MaxPool2d(kernel_size=4, stride=4),
             nn.Dropout(p=dropout)
         )
+
+        # Audio Transformer block
+        self.transf_maxpool = nn.MaxPool2d(kernel_size=[2,4], stride=[2,4])
+        transf_layer = nn.TransformerEncoderLayer(d_model=64, nhead=4, dim_feedforward=dim_feedforward, dropout=dropout, activation='relu')
+        self.transf_encoder = nn.TransformerEncoder(transf_layer, num_layers=4)
+
+        # Facial Transformer block
+        self.transf_maxpool_facial = nn.MaxPool2d(kernel_size=[1,4], stride=[1,2])
+        transf_layer_facial = nn.TransformerEncoderLayer(d_model=35, nhead=5, dim_feedforward=dim_feedforward, dropout=dropout, activation='relu')
+        self.transf_encoder_facial = nn.TransformerEncoder(transf_layer_facial, num_layers=4)
+
+
         # LSTM block
-        self.lstm_maxpool = nn.MaxPool2d(kernel_size=[2,4], stride=[2,4])
-        # hidden_size = 128
-        self.lstm = nn.LSTM(input_size=64,hidden_size=rnn_size,bidirectional=True, batch_first=True)
-        self.dropout_lstm = nn.Dropout(dropout)
-        self.attention_linear = nn.Linear(2*rnn_size,1) # 2*hidden_size for the 2 outputs of bidir LSTM
+        # self.lstm_maxpool = nn.MaxPool2d(kernel_size=[2,4], stride=[2,4])
+        # self.lstm = nn.LSTM(input_size=64,hidden_size=rnn_size,bidirectional=True, batch_first=True)
+        # self.dropout_lstm = nn.Dropout(dropout)
+        # self.attention_linear = nn.Linear(2*rnn_size,1) # 2*hidden_size for the 2 outputs of bidir LSTM
+
+
         # Linear softmax layer
         # self.linear_in = hidden_size
         self.out_linear = nn.LazyLinear(num_emotions) #2*hidden_size+192,
         self.dropout_linear = nn.Dropout(p=dropout)
         self.out_softmax = nn.Softmax(dim=1)
-    def forward(self,x2,x):
+    def forward(self,x1,x2,z):
+        # audio features
         # conv embedding
-        conv_embedding = self.conv2Dblock(x) #(b,channel,freq,time)
+        conv_embedding = self.conv2Dblock(x2) #(b,channel,freq,time)
         conv_embedding = torch.flatten(conv_embedding, start_dim=1) # do not flatten batch dimension
-        # lstm embedding
-        x_reduced = self.lstm_maxpool(x)
+
+        # transformer embedding
+        x_reduced = self.transf_maxpool(x2)
         x_reduced = torch.squeeze(x_reduced,1)
-        x_reduced = x_reduced.permute(0,2,1) # (b,t,freq)
-        lstm_embedding, (h,c) = self.lstm(x_reduced) # (b, time, hidden_size*2)
-        lstm_embedding = self.dropout_lstm(lstm_embedding)
-        batch_size,T,_ = lstm_embedding.shape 
-        attention_weights = [None]*T
-        for t in range(T):
-            embedding = lstm_embedding[:,t,:]
-            attention_weights[t] = self.attention_linear(embedding)
-        attention_weights_norm = nn.functional.softmax(torch.stack(attention_weights,-1),-1)
-        attention = torch.bmm(attention_weights_norm,lstm_embedding) # (Bx1xT)*(B,T,hidden_size*2)=(B,1,2*hidden_size)
-        attention = torch.squeeze(attention, 1)
+        x_reduced = x_reduced.permute(2,0,1) # requires shape = (time,batch,embedding)
+        transf_out = self.transf_encoder(x_reduced)
+        transf_embedding = torch.mean(transf_out, dim=0)
+
+        # # lstm embedding
+        # x_reduced = self.lstm_maxpool(x2)
+        # x_reduced = torch.squeeze(x_reduced,1)
+        # x_reduced = x_reduced.permute(0,2,1) # (b,t,freq)
+        # lstm_embedding, (h,c) = self.lstm(x_reduced) # (b, time, hidden_size*2)
+        # lstm_embedding = self.dropout_lstm(lstm_embedding)
+        # batch_size,T,_ = lstm_embedding.shape 
+        # attention_weights = [None]*T
+        # for t in range(T):
+        #     embedding = lstm_embedding[:,t,:]
+        #     attention_weights[t] = self.attention_linear(embedding)
+        # attention_weights_norm = nn.functional.softmax(torch.stack(attention_weights,-1),-1)
+        # attention = torch.bmm(attention_weights_norm,lstm_embedding) # (Bx1xT)*(B,T,hidden_size*2)=(B,1,2*hidden_size)
+        # attention = torch.squeeze(attention, 1)
+
+
         # concatenate
-        complete_embedding = torch.cat([conv_embedding, attention,x2.squeeze()], dim=1) 
+        complete_embedding = torch.cat([transf_embedding,conv_embedding,x1.squeeze()],dim=1)
         
         # self.linear_in = complete_embedding.shape[1]
         output_logits = self.out_linear(complete_embedding)
         output_logits = self.dropout_linear(output_logits)
         output_softmax = self.out_softmax(output_logits)
-        return output_logits, output_softmax, attention_weights_norm
+        return output_logits, output_softmax#, attention_weights_norm
 
 
 def loss_fnc(predictions, targets):
     return nn.CrossEntropyLoss()(input=predictions,target=targets)
 
 def make_train_step(model, loss_fnc, optimizer):
-    def train_step(X1,X2,Y):
+    def train_step(X1,X2,Z,Y):
         # set model to train mode
         model.train()
         # forward pass
-        output_logits, output_softmax, attention_weights_norm = model(X1,X2)
+        output_logits, output_softmax = model(X1,X2,Z)
         predictions = torch.argmax(output_softmax,dim=1)
         accuracy = torch.sum(Y==predictions)/float(len(Y))
         # compute loss
@@ -113,10 +225,10 @@ def make_train_step(model, loss_fnc, optimizer):
     return train_step
 
 def make_validate_fnc(model,loss_fnc):
-    def validate(X1,X2,Y):
+    def validate(X1,X2,Z,Y):
         with torch.no_grad():
             model.eval()
-            output_logits, output_softmax, attention_weights_norm = model(X1,X2)
+            output_logits, output_softmax = model(X1,X2,Z)
             predictions = torch.argmax(output_softmax,dim=1)
             accuracy = torch.sum(Y==predictions)/float(len(Y))
             loss = loss_fnc(output_logits,Y)
